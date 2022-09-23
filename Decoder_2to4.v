973faaf0c8818aa889b276b1d44f40d56bed3f0e
KhanhEK <107037350+KhanhEK2846@users.noreply.github.com>
2022-09-23 07:50:03 +0700

    Update Decoder_2to4.v

--- Decoder_2to4.v
+++ Decoder_2to4.v
@@ -3,7 +3,7 @@ input [1:0]I;
 input En;
 output [3:0]Y;
 	assign	Y[0] = ~I[0] & ~I[1] & En,
-				Y[1] = I[0] & ~I[1] & En,
-				Y[2] = ~I[0] & I[1] & En,
-				Y[3] = I[0] & I[1] & En;			
+		Y[1] = I[0] & ~I[1] & En,
+		Y[2] = ~I[0] & I[1] & En,
+		Y[3] = I[0] & I[1] & En;			
 endmodule
